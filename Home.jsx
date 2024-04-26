806c664496b874e853e0e1967611ca463c83dd39
Reni98 <93825160+Reni98@users.noreply.github.com>
2024-04-26 10:33:27 +0200

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../src/home.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+         <h1>Sorozatok egy helyen!</h1>
+    <p>Nézzen körül, hatalmas kínálatunkban</p>
+    </div>
+   
+
+  )
+}
+
+export default Home
